7cbf28eff5994a297104748cd471d1a5d1415630
Mohamed WLN <114567817+MDDiallo9@users.noreply.github.com>
2023-11-17 10:27:09 +0100

    Ajout des liens vers les 4 pages dans la nav

---
+++ views/partials/nav.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+        <li><a href="/">Accueil</a></li>
+        <li><a href="/">Photos</a></li>
+        <li><a href="/">Prix</a></li>
+        <li><a href="/">Admin</a></li>
+    </ul>
+</nav>
