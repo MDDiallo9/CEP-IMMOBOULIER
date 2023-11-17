45458698da1702541afd23a2de14142085d93c40
Mohamed WLN <114567817+MDDiallo9@users.noreply.github.com>
2023-11-17 10:55:05 +0100

    Ajout du lien vers la page prix

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -2,7 +2,7 @@
     <ul>
         <li><a href="/">Accueil</a></li>
         <li><a href="/">Photos</a></li>
-        <li><a href="/">Prix</a></li>
+        <li><a href="/prix">Prix</a></li>
         <li><a href="/">Admin</a></li>
     </ul>
 </nav>
