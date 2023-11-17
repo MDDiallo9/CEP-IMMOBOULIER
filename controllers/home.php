6ea7fbd0843034ced694284f3e4b15553149e25f
Mohamed WLN <114567817+MDDiallo9@users.noreply.github.com>
2023-11-17 10:24:12 +0100

    Ajout d'une boucle pour afficher les annonces

--- controllers/home.php
+++ controllers/home.php
@@ -1,3 +1,6 @@
 <?php
+require ("./models/Database.php");
+$annonces = $db->query("SELECT * FROM annonce ORDER BY cree_le DESC")->fetchAll();
 
+debug($annonces);
 require("./views/home.view.php");
--- views/home.view.php
+++ views/home.view.php
@@ -1,1 +1,21 @@
-<h1>Homepage</h1>
+<?php require("partials/header.php"); ?>
+
+
+<?php foreach ($annonces as $annonce) {
+?>
+    <article>
+        <h2><?= $annonce["titre"] ?></h2>
+        <?php if ($annonce["type"] === 0) { ?>
+            <p class="type">Location</p>
+        <?php } else { ?>
+            <p class="type">Achat</p>
+        <?php } ?>
+        <div><img src="" alt=""></div>
+        <p><?= $annonce["description"] ?></p>
+        <p class="surface"><?= $annonce["surface"] ?>m² , <?= $annonce["pieces"] ?> pièces </p>
+        <p><strong><?= $annonce["prix"] ?> €</strong></p>
+        <p class="energie">Energie : <?= $annonce["energie"] ?> Pollution : <?= $annonce["pollution"] ?></p>
+    </article>
+<?php  }; ?>
+
+<?php require("partials/footer.php"); ?>
