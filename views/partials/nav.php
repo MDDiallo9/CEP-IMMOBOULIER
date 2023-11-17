75ccc6563e662594580b1c12d18b1e10db4b7627
Mohamed WLN <114567817+MDDiallo9@users.noreply.github.com>
2023-11-17 14:07:33 +0100

    Ajout lien vers dashboard

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -1,8 +1,9 @@
 <nav>
+    <h1>Immoboulier</h1>
     <ul>
         <li><a href="/">Accueil</a></li>
         <li><a href="/">Photos</a></li>
         <li><a href="/prix">Prix</a></li>
-        <li><a href="/">Admin</a></li>
+        <li><a href="/admin">Admin</a></li>
     </ul>
 </nav>
